1d27d87e242eb88114aeff82c5c5c33f719a3d64
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-07-29 22:56:51 +0700

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,7 @@ class AuthController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
+        return auth()->shouldUse('api');
     }
 
     /**
