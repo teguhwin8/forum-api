a47903c6fffe4cee77814cfa45f583658a446773
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-07-29 22:11:02 +0700

    jwt auth config

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -49,5 +50,15 @@ class User extends Authenticatable
     public function forumComments()
     {
         return $this->hasMany(ForumComment::class);
+    }
+    
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
