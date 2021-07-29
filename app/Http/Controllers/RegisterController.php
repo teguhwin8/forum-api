89dfa744867d855f14a0c8b54877a1c11b2389f7
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-07-29 22:51:53 +0700

    validator register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     public function register()
     {
-        $this->validate(request(), [
+        $validator = Validator::make(request()->all(), [
             'username' => 'required|unique:users',
             'email' => 'required|email|unique:users',
             'password' => 'required',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages());
+        }
 
         $user = User::create([
             'username' => request('username'),
